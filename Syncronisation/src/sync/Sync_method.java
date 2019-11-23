194ec00893559025656c85382fd190395ce2d25d
HP <HP@A13AnujBansal>
2019-11-23 17:00:12 +0530

    Syncronisation

---
+++ Syncronisation/src/sync/Sync_method.java
@@ -0,0 +1,33 @@
+package sync;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Sync_method {
+
+	public static WebElement ElementPresent(WebDriver driver,String xpath,int time) 
+	{
+		WebElement ele = null;
+		
+		for(int i=0;i<time;i++)
+		{
+			try {
+				ele=driver.findElement(By.xpath(xpath));
+				break;
+			}
+			catch(Exception e)
+			{
+			try
+			{
+			Thread.sleep(1000);
+			} catch (InterruptedException e1)
+			{
+				System.out.println("Waiting for element to appear on DOM");
+			}
+			}
+		}
+			return ele;
+	}
+
+}

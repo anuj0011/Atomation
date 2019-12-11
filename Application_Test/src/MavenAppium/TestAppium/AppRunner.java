d8e7c09b1de9f503735cccafbc55e2aa3d3a5d60
HP <HP@A13AnujBansal>
2019-12-11 18:01:04 +0530

    changes

--- Application_Test/src/MavenAppium/TestAppium/AppRunner.java
+++ Application_Test/src/MavenAppium/TestAppium/AppRunner.java
@@ -5,11 +5,15 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
@@ -29,7 +33,7 @@ public class AppRunner {
 		
 		AndroidDriver<WebElement> driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
 
-		Thread.sleep(7000);
+		Thread.sleep(15000);
 
 		// List<MobileElement> editText =
 		// driver.findElements(By.className("android.widget.LinearLayout"));
@@ -40,21 +44,43 @@ public class AppRunner {
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Australia']")).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Submit']")).click();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); */
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);*/
 		driver.findElement(By.xpath("//android.widget.TextView[@text='What are you looking for?']")).click();
 		
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    Actions action = new Actions(driver);
 	    action.sendKeys("lenovo").perform();
-	    
+	    action.sendKeys(Keys.ENTER).perform();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//android.widget.RelativeLayout[@index='2']")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.id("com.ubuy:id/add.to.cart")).click();
+		driver.findElement(By.xpath("//android.widget.RelativeLayout[@index='1']")).click();
+		Thread.sleep(7000);		
+		TouchActions touch = new TouchActions(driver);
+		touch.scroll(10, 100);
+		action.perform();
+
+
+		
+		driver.findElement(By.id("com.ubuy:id/check.out")).click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//android.view.View[@text='Login']")).click();
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.id("com.ubuy:id/et_email")).click();
+		action.sendKeys("anuj.bansal@ubuy.co.in").perform();
+		driver.findElement(By.id("com.ubuy:id/et_password")).click();
+		action.sendKeys("00ubuy11").perform();
+		
+		driver.findElement(By.id("com.ubuy:id/sing.in")).click();
 
 		
 
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.findElement(By.id("com.ubuy:id/imageView4")).click();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//android.widget.LinearLayout[contains(@resource-id,'com.ubuy:id/product_item')]")).click();
+
+
 
 
 

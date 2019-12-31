cee0a06a906f7753115b75460eb78e8d821fe107
HP <HP@A13AnujBansal>
2019-12-31 16:05:09 +0530

    changes

--- Application_Test/src/MavenAppium/TestAppium/AppRunner.java
+++ Application_Test/src/MavenAppium/TestAppium/AppRunner.java
@@ -2,9 +2,11 @@ package MavenAppium.TestAppium;
 
 import java.awt.Desktop.Action;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -14,11 +16,20 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.offset.PointOption;
+
+import java.time.Duration;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+
+
+
 
 public class AppRunner {
 
@@ -36,7 +47,7 @@ public class AppRunner {
 		
 		AndroidDriver<WebElement> driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
 
-		Thread.sleep(15000);
+		Thread.sleep(5000);
 
 		// List<MobileElement> editText =
 		// driver.findElements(By.className("android.widget.LinearLayout"));
@@ -51,8 +62,11 @@ public class AppRunner {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);*/
 		
 		driver.findElement(By.xpath("//android.widget.TextView[@text='US']")).click();
-
 		
+		//swipeVertical((AppiumDriver)driver,0.05,0.9,0.5,3000);
+
+		Thread.sleep(15000);
+
 		driver.findElement(By.xpath("//android.widget.TextView[@text='What are you looking for?']")).click();
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -111,16 +125,30 @@ public class AppRunner {
 		
 		driver.findElement(By.xpath("//android.view.View[@text='Visa/Mastercard']")).click();
 		
-		driver.findElement(By.id("cybersecure_cc_number")).click();
+		
+		driver.findElement(By.xpath("//android.widget.EditText[@index='0']")).click();
 		action.sendKeys("4377486577038084").perform();
+		Thread.sleep(2000);
+		
+		swipeVertical((AppiumDriver)driver,0.05,0.9,0.5,3000);
+		    
 		driver.findElement(By.id("cybersecure_cc_cid")).click();
 		action.sendKeys("562").perform();
 		
 		driver.findElement(By.xpath("//android.widget.Button[@text='Continue']")).click();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		
-
-
+	}
+	
+	// *** FOR SCROLLING/SWIPE ***
+	
+	public static void swipeVertical(AppiumDriver driver, double startPercentage, double finalPercentage, double anchorPercentage, int duration) throws Exception {
+        Dimension size = driver.manage().window().getSize();
+        int anchor = (int) (size.width * anchorPercentage);
+        int startPoint = (int) (size.height * startPercentage);
+        int endPoint = (int) (size.height * finalPercentage);
+        new TouchAction(driver).press(PointOption.point(anchor,startPoint)).waitAction(waitOptions(Duration.ofMillis(duration))).moveTo(PointOption.point(anchor, endPoint)).release().perform();
+        
 	}
 
 }
---
+++ UBUYA_CHROME/src/abroad_chrome/SearchOnly.java
@@ -0,0 +1,91 @@
+package abroad_chrome;
+
+import org.testng.annotations.Test;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xmlbeans.impl.xb.ltgfmt.TestsDocument.Tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+
+public class SearchOnly {
+
+	public static WebDriver driver;
+
+	@Test
+	public void f() throws InterruptedException, AWTException, IOException {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Lib\\chrome_driver_latest\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+	
+
+		FileInputStream file = new FileInputStream(new File("D:\\ExcelReadAutomated\\Normalsearchcheck.xlsx"));
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		driver.get("https://www.ubuy.co.in");
+		for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+			try {
+				
+				String keyword = sheet.getRow(i).getCell(0).getStringCellValue();
+				
+
+// NORMAL SEARCH
+			WebElement searchbox =	driver.findElement(By.xpath("//input[@autofocus='autofocus']"));
+			searchbox.clear();
+			searchbox.sendKeys(keyword);
+			searchbox.submit();
+				
+			Thread.sleep(20000);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				
+		
+			}
+
+		}
+	}
+}
+
+	
+	
+	
+
+	

14932465f1de99a60512b56ab00d090a8081fd53
anuj <anuj@anuj-HP-Slim-Desktop-290-p0xxx>
2020-10-02 16:47:37 +0530

    updatessss

--- Calubuy/src/calculate/Ubuyshipping.java
+++ Calubuy/src/calculate/Ubuyshipping.java
@@ -46,6 +46,21 @@ class MyFrame extends JFrame {
 	private JButton sub3;
 	private JButton reset3;
 	private JLabel res3;
+
+// quality analysis
+
+	private JLabel title4;
+	private JLabel dwei;
+	private JTextField tdwei;
+
+	private JLabel wei;
+	private JTextField twei;
+	private JLabel amz;
+	private JTextField tamz;
+
+	private JButton sub4;
+	private JButton reset4;
+	private JLabel res4;
 //note	
 	private JTextArea tout;
 
@@ -70,102 +85,102 @@ class MyFrame extends JFrame {
 
 //main shipping	
 
-		title = new JLabel("SHIPPING FINDER");
-		title.setFont(new Font("Arial", Font.PLAIN, 25));
+		title = new JLabel("SHIPPING FINDER:");
+		title.setFont(new Font("Arial", Font.PLAIN, 20));
 		title.setSize(300, 30);
-		title.setLocation(350, 30);
+		title.setLocation(100, 20);
 		c.add(title);
 
 		weight = new JLabel("Weight(pounds)");
-		weight.setFont(new Font("Arial", Font.PLAIN, 20));
+		weight.setFont(new Font("Arial", Font.PLAIN, 18));
 		weight.setSize(200, 25);
-		weight.setLocation(100, 100);
+		weight.setLocation(100, 60);
 		c.add(weight);
 
 		num1 = new JTextField();
 		num1.setFont(new Font("Arial", Font.PLAIN, 15));
 		num1.setSize(80, 25);
-		num1.setLocation(350, 100);
+		num1.setLocation(350, 60);
 		c.add(num1);
 
 		cart = new JLabel("No. of Items in Cart");
-		cart.setFont(new Font("Arial", Font.PLAIN, 20));
+		cart.setFont(new Font("Arial", Font.PLAIN, 18));
 		cart.setSize(200, 25);
-		cart.setLocation(100, 150);
+		cart.setLocation(100, 100);
 		c.add(cart);
 
 		num3 = new JTextField();
 		num3.setFont(new Font("Arial", Font.PLAIN, 15));
 		num3.setSize(80, 25);
-		num3.setLocation(350, 150);
+		num3.setLocation(350, 100);
 		c.add(num3);
 
 		prime = new JLabel("Any Non-Prime item?");
-		prime.setFont(new Font("Arial", Font.PLAIN, 20));
+		prime.setFont(new Font("Arial", Font.PLAIN, 18));
 		prime.setSize(250, 25);
-		prime.setLocation(100, 200);
+		prime.setLocation(100, 140);
 		c.add(prime);
 
 		nonprime = new JComboBox(nonprimes);
 		nonprime.setFont(new Font("Arial", Font.PLAIN, 15));
 		nonprime.setSize(75, 25);
-		nonprime.setLocation(350, 200);
+		nonprime.setLocation(350, 140);
 		c.add(nonprime);
 
 		country = new JLabel("Country");
-		country.setFont(new Font("Arial", Font.PLAIN, 20));
+		country.setFont(new Font("Arial", Font.PLAIN, 18));
 		country.setSize(100, 25);
-		country.setLocation(100, 250);
+		country.setLocation(100, 180);
 		c.add(country);
 
 		num2 = new JComboBox(countrys);
 		num2.setFont(new Font("Arial", Font.PLAIN, 15));
 		num2.setSize(125, 25);
-		num2.setLocation(200, 250);
+		num2.setLocation(200, 180);
 		c.add(num2);
 
 		store = new JLabel("Store");
-		store.setFont(new Font("Arial", Font.PLAIN, 20));
+		store.setFont(new Font("Arial", Font.PLAIN, 18));
 		store.setSize(100, 25);
-		store.setLocation(100, 300);
+		store.setLocation(100, 220);
 		c.add(store);
 
 		stores = new JComboBox(str);
 		stores.setFont(new Font("Arial", Font.PLAIN, 15));
 		stores.setSize(50, 25);
-		stores.setLocation(200, 300);
+		stores.setLocation(200, 220);
 		c.add(stores);
 
 		shipping = new JLabel("Shipping Method");
-		shipping.setFont(new Font("Arial", Font.PLAIN, 20));
+		shipping.setFont(new Font("Arial", Font.PLAIN, 18));
 		shipping.setSize(200, 25);
-		shipping.setLocation(100, 350);
+		shipping.setLocation(100, 260);
 		c.add(shipping);
 
 		shippingtype = new JComboBox(ship);
 		shippingtype.setFont(new Font("Arial", Font.PLAIN, 15));
 		shippingtype.setSize(100, 20);
-		shippingtype.setLocation(300, 350);
+		shippingtype.setLocation(300, 260);
 		c.add(shippingtype);
 
 		sub = new JButton("Submit");
 		sub.setFont(new Font("Arial", Font.PLAIN, 15));
 		sub.setSize(100, 20);
-		sub.setLocation(150, 390);
+		sub.setLocation(150, 300);
 		sub.addActionListener(b1Event);
 		c.add(sub);
 
 		reset = new JButton("Reset");
 		reset.setFont(new Font("Arial", Font.PLAIN, 15));
 		reset.setSize(100, 20);
-		reset.setLocation(270, 390);
+		reset.setLocation(270, 300);
 		reset.addActionListener(b1Event);
 		c.add(reset);
 
 		res = new JLabel("");
 		res.setFont(new Font("Arial", Font.PLAIN, 20));
 		res.setSize(500, 25);
-		res.setLocation(125, 425);
+		res.setLocation(125, 335);
 		c.add(res);
 
 		setVisible(true);
@@ -175,63 +190,63 @@ class MyFrame extends JFrame {
 		title2 = new JLabel("Inches to Pounds:");
 		title2.setFont(new Font("Arial", Font.PLAIN, 20));
 		title2.setSize(300, 20);
-		title2.setLocation(550, 100);
+		title2.setLocation(550, 20);
 		c.add(title2);
 
 		length = new JLabel("L");
-		length.setFont(new Font("Arial", Font.PLAIN, 20));
+		length.setFont(new Font("Arial", Font.PLAIN, 18));
 		length.setSize(50, 25);
-		length.setLocation(550, 150);
+		length.setLocation(550, 60);
 		c.add(length);
 
 		tlength = new JTextField();
 		tlength.setFont(new Font("Arial", Font.PLAIN, 15));
 		tlength.setSize(50, 25);
-		tlength.setLocation(575, 150);
+		tlength.setLocation(575, 60);
 		c.add(tlength);
 
 		bredth = new JLabel("B");
-		bredth.setFont(new Font("Arial", Font.PLAIN, 20));
+		bredth.setFont(new Font("Arial", Font.PLAIN, 18));
 		bredth.setSize(50, 25);
-		bredth.setLocation(635, 150);
+		bredth.setLocation(635, 60);
 		c.add(bredth);
 
 		tbredth = new JTextField();
 		tbredth.setFont(new Font("Arial", Font.PLAIN, 15));
 		tbredth.setSize(50, 25);
-		tbredth.setLocation(660, 150);
+		tbredth.setLocation(660, 60);
 		c.add(tbredth);
 
 		height = new JLabel("H");
-		height.setFont(new Font("Arial", Font.PLAIN, 20));
+		height.setFont(new Font("Arial", Font.PLAIN, 18));
 		height.setSize(50, 25);
-		height.setLocation(720, 150);
+		height.setLocation(720, 60);
 		c.add(height);
 
 		theight = new JTextField();
 		theight.setFont(new Font("Arial", Font.PLAIN, 15));
 		theight.setSize(50, 25);
-		theight.setLocation(745, 150);
+		theight.setLocation(745, 60);
 		c.add(theight);
 
 		sub2 = new JButton("Submit");
 		sub2.setFont(new Font("Arial", Font.PLAIN, 15));
 		sub2.setSize(90, 20);
-		sub2.setLocation(575, 200);
+		sub2.setLocation(575, 100);
 		sub2.addActionListener(b2Event);
 		c.add(sub2);
 
 		reset2 = new JButton("Reset");
 		reset2.setFont(new Font("Arial", Font.PLAIN, 15));
 		reset2.setSize(90, 20);
-		reset2.setLocation(675, 200);
+		reset2.setLocation(675, 100);
 		reset2.addActionListener(b2Event);
 		c.add(reset2);
 
 		res2 = new JLabel("");
 		res2.setFont(new Font("Arial", Font.PLAIN, 20));
 		res2.setSize(500, 25);
-		res2.setLocation(550, 225);
+		res2.setLocation(550, 125);
 		c.add(res2);
 
 		setVisible(true);
@@ -241,40 +256,106 @@ class MyFrame extends JFrame {
 		title3 = new JLabel("Kg to Pounds:");
 		title3.setFont(new Font("Arial", Font.PLAIN, 20));
 		title3.setSize(300, 25);
-		title3.setLocation(550, 280);
+		title3.setLocation(550, 180);
 		c.add(title3);
 
 		kg = new JLabel("Kg");
-		kg.setFont(new Font("Arial", Font.PLAIN, 20));
+		kg.setFont(new Font("Arial", Font.PLAIN, 18));
 		kg.setSize(50, 25);
-		kg.setLocation(550, 330);
+		kg.setLocation(550, 220);
 		c.add(kg);
 
 		tkg = new JTextField();
 		tkg.setFont(new Font("Arial", Font.PLAIN, 15));
 		tkg.setSize(50, 25);
-		tkg.setLocation(585, 330);
+		tkg.setLocation(585, 220);
 		c.add(tkg);
 
 		sub3 = new JButton("Submit");
 		sub3.setFont(new Font("Arial", Font.PLAIN, 15));
 		sub3.setSize(90, 20);
-		sub3.setLocation(575, 390);
+		sub3.setLocation(575, 260);
 		sub3.addActionListener(b3Event);
 		c.add(sub3);
 
 		reset3 = new JButton("Reset");
 		reset3.setFont(new Font("Arial", Font.PLAIN, 15));
 		reset3.setSize(90, 20);
-		reset3.setLocation(675, 390);
+		reset3.setLocation(675, 260);
 		reset3.addActionListener(b3Event);
 		c.add(reset3);
 
 		res3 = new JLabel("");
 		res3.setFont(new Font("Arial", Font.PLAIN, 20));
 		res3.setSize(500, 25);
-		res3.setLocation(550, 425);
+		res3.setLocation(550, 295);
 		c.add(res3);
+
+		setVisible(true);
+
+// operation analysis
+
+		title4 = new JLabel("Operation Analysis:");
+		title4.setFont(new Font("Arial", Font.PLAIN, 20));
+		title4.setSize(300, 25);
+		title4.setLocation(550, 330);
+		c.add(title4);
+
+		dwei = new JLabel("Dimentional(Weight)");
+		dwei.setFont(new Font("Arial", Font.PLAIN, 18));
+		dwei.setSize(250, 25);
+		dwei.setLocation(550, 370);
+		c.add(dwei);
+
+		tdwei = new JTextField();
+		tdwei.setFont(new Font("Arial", Font.PLAIN, 15));
+		tdwei.setSize(70, 25);
+		tdwei.setLocation(750, 370);
+		c.add(tdwei);
+
+		wei = new JLabel("Weight");
+		wei.setFont(new Font("Arial", Font.PLAIN, 18));
+		wei.setSize(100, 25);
+		wei.setLocation(550, 410);
+		c.add(wei);
+
+		twei = new JTextField();
+		twei.setFont(new Font("Arial", Font.PLAIN, 15));
+		twei.setSize(75, 25);
+		twei.setLocation(550, 440);
+		c.add(twei);
+
+		amz = new JLabel("Price(Amazon)");
+		amz.setFont(new Font("Arial", Font.PLAIN, 18));
+		amz.setSize(150, 25);
+		amz.setLocation(700, 410);
+		c.add(amz);
+
+		tamz = new JTextField();
+		tamz.setFont(new Font("Arial", Font.PLAIN, 15));
+		tamz.setSize(75, 25);
+		tamz.setLocation(700, 440);
+		c.add(tamz);
+
+		sub4 = new JButton("Submit");
+		sub4.setFont(new Font("Arial", Font.PLAIN, 15));
+		sub4.setSize(90, 20);
+		sub4.setLocation(575, 480);
+		sub4.addActionListener(b4Event);
+		c.add(sub4);
+
+		reset4 = new JButton("Reset");
+		reset4.setFont(new Font("Arial", Font.PLAIN, 15));
+		reset4.setSize(90, 20);
+		reset4.setLocation(675, 480);
+		reset4.addActionListener(b4Event);
+		c.add(reset4);
+
+		res4 = new JLabel("");
+		res4.setFont(new Font("Arial", Font.PLAIN, 11));
+		res4.setSize(500, 25);
+		res4.setLocation(475, 515);
+		c.add(res4);
 
 		setVisible(true);
 
@@ -283,10 +364,10 @@ class MyFrame extends JFrame {
 		tout = new JTextArea(
 				"*NOTE* 1. IF items have eau de, parfum,Perfume,edp,edt,spray,Rogaine,Rust oleum,paint,par-feme,body mist,fragrance mist,cologne,nail polish keywords in name then add 18$ in shipping price.                   2. If items have battery,batteries,li ion,li-ion, lithium ion, lithium metal,corrosive,flammable,aerosol,hazardous,hazard,rust oleum,paint,Co2,Pistol keywords in name then add 20$ in shipping price.");
 		tout.setFont(new Font("Arial", Font.PLAIN, 15));
-		tout.setSize(750, 175);
-		tout.setLocation(100, 475);
+		tout.setSize(350, 250);
+		tout.setLocation(100, 390);
 		tout.setLineWrap(true);
-		tout.setEditable(false);
+		tout.setEditable(true);
 		c.add(tout);
 
 	}
@@ -301,6 +382,8 @@ class MyFrame extends JFrame {
 	double china;
 	double v;
 	double g;
+	double f;
+	double j;
 
 	ActionListener b1Event = new ActionListener() {
 
@@ -543,6 +626,182 @@ class MyFrame extends JFrame {
 				String p = "";
 				tkg.setText(p);
 				res3.setText(p);
+			}
+
+		}
+
+	};
+
+//quality analysis
+
+	ActionListener b4Event = new ActionListener() {
+
+		public void actionPerformed(ActionEvent z) {
+
+			String text6 = tdwei.getText();
+			double ttdwei = Double.parseDouble(text6);
+
+			String text7 = twei.getText();
+			double ttwei = Double.parseDouble(text7);
+
+			String text8 = tamz.getText();
+			double ttamz = Double.parseDouble(text8);
+
+			if (z.getSource() == sub4) {
+
+				if (ttamz < 26) {
+
+					f = 5 * ttwei;
+					if (f < ttdwei) {
+
+						res4.setText("New Weight(Pounds)" + f);
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz >= 26 && ttamz <= 50) {
+
+					f = 5 * ttwei;
+					if (f < ttdwei) {
+
+						f = f + 3;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $10 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 50 && ttamz < 76) {
+					f = ttwei * 4;
+					if (f < ttdwei) {
+
+						f = f + 5;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $15 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz >= 76 && ttamz <= 80) {
+					f = ttwei * 4;
+					if (f < ttdwei) {
+
+						f = f + 6;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $20 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 80 && ttamz <= 100) {
+					f = ttwei * 4;
+					if (f < ttdwei) {
+
+						f = f + 10;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $25 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 100 && ttamz <= 125) {
+					f = ttwei * 3;
+					if (f < ttdwei) {
+
+						f = f + 13;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $30 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 125 && ttamz <= 150) {
+					f = ttwei * 3;
+					if (f < ttdwei) {
+
+						f = f + 16;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $30 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 150 && ttamz <= 200) {
+					f = ttwei * 3;
+					if (f < ttdwei) {
+
+						f = f + 20;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $30 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 200 && ttamz <= 300) {
+					f = ttwei * 3;
+					if (f < ttdwei) {
+
+						f = f + 30;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $30 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 300 && ttamz <= 500) {
+					f = ttwei * 2.5;
+					if (f < ttdwei) {
+
+						f = f + 25;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra $50 in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 500 && ttamz <= 1000) {
+					f = ttwei * 2.5;
+					if (f < ttdwei) {
+						j = ttamz * .05;
+						j = j + 50;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra " + j + " product price in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 1000 && ttamz <= 2000) {
+					f = ttwei * 2.5;
+					if (f < ttdwei) {
+						j = ttamz * .06;
+						j = j + 50;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra " + j + " product price in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				} else if (ttamz > 2000 && ttamz <= 5000) {
+					f = ttwei * 2.5;
+					if (f < ttdwei) {
+						j = ttamz * .06;
+						j = j + 100;
+						res4.setText("New Weight(Pounds)" + f + ",Add extra " + j + " product price in shipping price");
+
+					} else {
+						res4.setText("Weight(Pounds)" + ttdwei);
+					}
+
+				}
+
+			} else if (z.getSource() == reset4) {
+
+				String m = "";
+				tdwei.setText(m);
+				twei.setText(m);
+				tamz.setText(m);
+				res4.setText(m);
 			}
 
 		}
